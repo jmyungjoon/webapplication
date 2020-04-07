30bad2ba313a98bebe1a856338ba0ff877ad3911
jmyungjoon <jmyungjoon@gmail.com>
2020-04-08 01:40:40 +0800

    google service

--- quickstart.php
+++ quickstart.php
@@ -12,8 +12,8 @@ if (php_sapi_name() != 'cli') {
 function getClient()
 {
     $client = new Google_Client();
-    $client->setApplicationName('Gmail API PHP Quickstart');
-    $client->setScopes(Google_Service_Gmail::GMAIL_READONLY);
+    $client->setApplicationName('Google Calendar API PHP Quickstart');
+    $client->setScopes(Google_Service_Calendar::CALENDAR_READONLY);
     $client->setAuthConfig('credentials.json');
     $client->setAccessType('offline');
     $client->setPrompt('select_account consent');
@@ -61,17 +61,28 @@ function getClient()
 
 // Get the API client and construct the service object.
 $client = getClient();
-$service = new Google_Service_Gmail($client);
+$service = new Google_Service_Calendar($client);
 
-// Print the labels in the user's account.
-$user = 'me';
-$results = $service->users_labels->listUsersLabels($user);
+// Print the next 10 events on the user's calendar.
+$calendarId = 'primary';
+$optParams = array(
+  'maxResults' => 10,
+  'orderBy' => 'startTime',
+  'singleEvents' => true,
+  'timeMin' => date('c'),
+);
+$results = $service->events->listEvents($calendarId, $optParams);
+$events = $results->getItems();
 
-if (count($results->getLabels()) == 0) {
-  print "No labels found.\n";
+if (empty($events)) {
+    print "No upcoming events found.\n";
 } else {
-  print "Labels:\n";
-  foreach ($results->getLabels() as $label) {
-    printf("- %s\n", $label->getName());
-  }
+    print "Upcoming events:\n";
+    foreach ($events as $event) {
+        $start = $event->start->dateTime;
+        if (empty($start)) {
+            $start = $event->start->date;
+        }
+        printf("%s (%s)\n", $event->getSummary(), $start);
+    }
 }
